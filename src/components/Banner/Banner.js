488764b000c4d2952028b08f2646d3baa1e432c9
BayajidAlam <bayajidalam2001@gmail.com>
2023-03-05 10:31:49 +0600

    banner added

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,80 +1,63 @@
 import React from "react";
-import logo from '../../assets/karim-manjra-cW3nDFVpi10-unsplash.jpg';
+import img1 from "../../assets/daniel-bernard-rNkhV94Xo0w-unsplash.jpg";
+import img2 from "../../assets/daniel-bernard-rNkhV94Xo0w-unsplash.jpg";
+import img3 from "../../assets/daniel-bernard-rNkhV94Xo0w-unsplash.jpg";
+import img4 from "../../assets/daniel-bernard-rNkhV94Xo0w-unsplash.jpg";
+import img5 from "../../assets/daniel-bernard-rNkhV94Xo0w-unsplash.jpg";
+import img6 from "../../assets/daniel-bernard-rNkhV94Xo0w-unsplash.jpg";
+import "./Banner.css";
+import BannerItem from "./BannerItem";
+
+const bannerData = [
+  {
+      image: img1,
+      prev: 6,
+      id: 1,
+      next: 2
+  },
+  {
+      image: img2,
+      prev: 1,
+      id: 2,
+      next: 3
+  },
+  {
+      image: img3,
+      prev: 2,
+      id: 3,
+      next: 4
+  },
+  {
+      image: img4,
+      prev: 3,
+      id: 4,
+      next: 5
+  },
+  {
+      image: img5,
+      prev: 4,
+      id: 5,
+      next: 6
+  },
+  {
+      image: img6,
+      prev: 5,
+      id: 6,
+      next: 1
+  }
+]
 
 const Banner = () => {
   return (
     <section>
       <div className="carousel container mx-auto mt-8 h-[550px]">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img
-            src={logo}
-            className="w-full"
-          alt=""/>
-          <div className="absolute flex justify-between transform  left-5 right-5 top-1/3">
-              <div className="w-1/2 mx-auto space-y-6">
-                <p className="text-6xl text-white font-bold text-center">Musical Equipment <br /> & Accessories</p>
-                <p className="text-white mx-auto text-xl w-2/3">Here you'll find a huge range of secondhand musical equipment from the biggest world famous brands</p>
-                <div className="flex items-center justify-center">
-                <button className='uppercase bg-main text-white py-2 px-6 mr-4 font-bold'>Shop now</button>
-                <button className='uppercase bg-black text-white py-2 px-6 font-bold'>View items</button>
-                </div>
-              </div>
-          </div>
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide4" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide2" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-
-        <div id="slide2" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1609621838510-5ad474b7d25d.jpg"
-            className="w-full"
-            alt=""
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide1" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide3" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1414694762283-acccc27bca85.jpg"
-            className="w-full"
-            alt=""
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide2" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide4" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img
-            src="/images/stock/photo-1665553365602-b2fb8e5d1707.jpg"
-            className="w-full"
-            alt=""
-          />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-            <a href="#slide3" className="btn btn-circle">
-              ❮
-            </a>
-            <a href="#slide1" className="btn btn-circle">
-              ❯
-            </a>
-          </div>
-        </div>
+     
+        {
+          bannerData.map(slide=><BannerItem
+          key={slide.id}
+          slide={slide}
+          ></BannerItem>)
+        }
       </div>
     </section>
   );
---
+++ src/components/Banner/BannerItem.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+const BannerItem = ({slide}) => {
+
+  const { image, prev,next, id } = slide
+
+
+  return (
+    <div id={`slide${id}`} className="carousel-item relative w-full">
+      <div className="img-gradient">
+        <img src={image} className="w-full banner-img" alt="" />
+      </div>
+      <div className="absolute flex justify-between transform  left-5 right-5 top-1/3">
+        <div className="w-1/2 mx-auto space-y-6">
+          <p className="text-6xl text-white font-bold text-center">
+            Musical Equipment <br /> & Accessories
+          </p>
+          <p className="text-white mx-auto text-xl w-2/3">
+            Here you'll find a huge range of secondhand musical equipment from
+            the biggest world famous brands
+          </p>
+          <div className="flex items-center justify-center">
+            <button className="uppercase bg-main text-white py-2 px-6 mr-4 font-bold">
+              Shop now
+            </button>
+            <button className="uppercase bg-black text-white py-2 px-6 font-bold">
+              View items
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href={`#slide${prev}`} className="btn btn-circle">
+          ❮
+        </a>
+        <a href={`#slide${next}`}  className="btn btn-circle">
+          ❯
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default BannerItem;
